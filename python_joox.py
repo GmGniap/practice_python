da7968ab4ec3dc70758eb3a92279e3251f837c67
gmgniap <thetpaimyo103@gmail.com>
2019-06-28 15:02:44 +0630

    Downloader script added!

--- python_joox.py
+++ python_joox.py
@@ -1,8 +1,30 @@
 import re
+import urllib.request
+#with open("C:\\Users\\User\\Desktop\\test.txt", "r", encoding="utf8") as file:
 
-with open("C:\\Users\\User\\Desktop\\test.txt", "r", encoding="utf8") as file:
+def search(word, sentences):
+    return [i for i in sentences if re.search(r'\b%s\b' % word, i)]
+
+#joox = "https://www.joox.com/mm/single/TdWPTjFLqphCV43Y04IlnA=="
+joox = input("Input URL Here!")
+id = joox.split('/')[-1]
+final = ('http://api.joox.com/web-fcgi-bin/web_get_songinfo?songid=' + id)
+urllib.request.urlretrieve(final, './Download/test.txt')
+print(id)
+print(final)
+
+with open("./Download/test.txt", "r", encoding="utf8") as file:
     for line in file:
-        urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', line)
-        new = re.findall('http[s]?://mm.stream.music.joox.com(<a +href="(.mp3+?)" *>)', line)
-        print(new)
-        print(urls)
+        #urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', line)
+        #news = re.findall('"((http|ftp)s?://.*?)"', line)
+        #for new in news:
+            #print(new[0])
+        #print(urls)'''
+
+        news = re.split('"((http|ftp)s?://.*?)"', line)
+        text = search('mp3', news)
+        break
+
+    #print(type(text))
+    url = text[0]
+    urllib.request.urlretrieve(url, './Download/app.mp3')
--- python_turtle.py
+++ python_turtle.py
@@ -5,10 +5,19 @@ alex = turtle.Turtle()
 alex.color("blue")
 alex.shape("turtle")
 
-print(list(range(5, 60, 2)))
+'''print(list(range(5, 60, 2)))
 alex.up()
 for col in range(5, 60, 2):
     alex.stamp()
     alex.forward(col)
     alex.right(24)
+wn.exitonclick()'''
+
+
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(100)
+
 wn.exitonclick()
