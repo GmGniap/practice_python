fc8a355541d9ce43d14914b47fd24c1da8468d99
gmgniap <thetpaing@utycc.edu.mm>
2022-05-22 16:30:11 +0630

    Deploy script update for template

--- 2022_code/deploy.sh
+++ 2022_code/deploy.sh
@@ -2,4 +2,6 @@
 datasette publish cloudrun price.db \
   --service burma-price \
   --metadata metadata.yml \
+  --static static:static/ \
+  --template-dir=templates/ \
   --extra-options="--setting sql_time_limit_ms 3000"
