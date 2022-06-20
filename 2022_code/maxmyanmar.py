b291414b6b8ba41c6bcfd707aae1d0f931904848
gmgniap <thetpaing@utycc.edu.mm>
2022-06-21 01:55:16 +0630

    MaxMyanmar code update

--- 2022_code/maxmyanmar.py
+++ 2022_code/maxmyanmar.py
@@ -1,8 +1,9 @@
-import imp
 from playwright.sync_api import sync_playwright
 import pandas as pd
 from datetime import date, datetime,timedelta
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
+import requests
+import json
 import re
 import sqlite3
 conn = sqlite3.connect("price.db")
@@ -90,7 +91,46 @@ def scrape_table(table):
     # print(all_rows)
     df = pd.DataFrame(data=all_rows, columns=heading)
     return df
-    
-if __name__ == "__main__":
-    scrape_daily_max()
 
+def scrape_old_data():
+    try:
+        url = "https://app.maxenergy.com.mm/maxapi/webapi/Price/GetPriceList"
+
+        payload = json.dumps({
+        "apikey": "R2wwQjRBdTFIbUY4OUFXRTZpbWZuYzhtVkxXd3NBYXdqWXI0Unh6YUNFTGdM",
+        "fromdate": "2020-01-14 12:00:00 AM",
+        "todate": "2020-01-14 11:00:00 PM"
+        })
+        headers = {
+        'content-type': 'application/json',
+        'Cookie': '.AspNetCore.Session=CfDJ8B0ta%2BbvQ0RLgbpaxcGdndgbCvK8BQSCNXPXosRd%2BsHPqTu3gVO7Z%2FTID1K2qyncCqw53HbvlUzTyAVYixfNnnZgMYT2siiOV1L0gzKGdYEe%2BYMldYcAlsYqWyohDi4g8t3Y49A%2FKGPoF4BHk1179zRtsuFT6ujfa6Zg8J%2Fxtzeg'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+        x = response.json()
+        raw_data = x['data']
+        raw_df = pd.json_normalize(raw_data)
+        
+        ## Generate CSV and JSON to check raw data
+        # raw_df.to_csv('./raw1.csv')
+        # with open('raw_data.json', 'w') as output:
+        #     json.dump(x, output)
+        
+        ## Take rows that not price Zero and reset index
+        clean_df = raw_df[raw_df['price'] != 0.0].reset_index(drop=True)
+        print("Done!")
+        
+        print(clean_df.shape)
+        print(clean_df.columns)
+        print(clean_df.head(10))
+    except ValueError as e:
+        print(f"Error : {e}")
+
+
+if __name__ == "__main__":
+    # scrape_daily_max()
+    # scrape_old_data()
+    first_date = datetime.strptime('2020-01-14', '%Y-%m-%d').date()
+    t = datetime.today().date()
+    if first_date != t:
+        print((t - first_date).days)
