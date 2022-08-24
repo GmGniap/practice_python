47230125bc6eb2f0b01be85307a95fa8cf10d09c
gmgniap <thetpaing@utycc.edu.mm>
2022-08-24 12:25:46 +0630

    python practice_Aug23

---
+++ Tutorials/general_python/my_module.py
@@ -0,0 +1,10 @@
+""" 
+This script is tested while following Python Workshop book.
+Make a Library in Python.
+"""
+
+from datetime import datetime
+current_t = datetime.now()
+
+if __name__ == "__main__":
+    print(current_t)
