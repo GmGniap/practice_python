e4a95aac0e408bdfc89a37f4536de5fb0d6feb6e
gmgniap <thetpaing@utycc.edu.mm>
2023-10-21 18:13:20 +0630

    Test sqlalchemy

--- Tutorials/flask_blog/main.py
+++ Tutorials/flask_blog/main.py
@@ -1,8 +1,33 @@
+from datetime import datetime
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '0e748eccc059ded6ce6e0d7cb3f6b302'   ## need for security (WTForms)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 import routes
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(25), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    img_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+    
+    def __repr__(self):
+        return f"User('{self.fullname}, '{self.email}')"
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 if __name__ == "__main__":
     app.run(debug=True)     ## For security, need to change to debug=False before deploy.
