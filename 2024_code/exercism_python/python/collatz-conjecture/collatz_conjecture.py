fd9a31fb099d35d6f3c4aac2fffe96a2e13a3a20
gmgniap <thetpaing@utycc.edu.mm>
2024-02-06 17:36:30 +0630

    Solved 2024_code/exercism_python/python/collatz-conjecture/collatz_conjecture.py

--- 2024_code/exercism_python/python/collatz-conjecture/collatz_conjecture.py
+++ 2024_code/exercism_python/python/collatz-conjecture/collatz_conjecture.py
@@ -1,7 +1,20 @@
-def steps(number):
+## Recursive solution , but doesn't work for Count
+def steps_recursive(number):
     print(f">> {number}")
     if number == 1:
         return
     return steps(number / 2) if number % 2 == 0 else steps((3 * number) + 1)
 
-steps(12)
+def steps(number):
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+    
+    count = 0
+    while number != 1:
+        if number % 2 == 0:
+            number /= 2
+        else:
+            number = (3 * number) + 1
+        count += 1
+    return count
+
