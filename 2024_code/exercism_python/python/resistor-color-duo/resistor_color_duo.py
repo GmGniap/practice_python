5e3d05d241762fb8ec0ad49e767840d3b2bdf7cb
gmgniap <thetpaing@utycc.edu.mm>
2024-02-11 15:51:40 +0630

    Solved more Exercism problems

--- 2024_code/exercism_python/python/resistor-color-duo/resistor_color_duo.py
+++ 2024_code/exercism_python/python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,16 @@
+    
 def value(colors):
-    pass
+    code_colors = [
+            "black",
+            "brown",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "violet",
+            "grey",
+            "white"
+        ]
+    return int("".join([str(code_colors.index(color)) for color in colors[:2]]))
+        
